7adaf337190d0ed45879ee64a41194763cfe6c05
Irina Giruta <irina.giruta@gmail.com>
2021-03-23 00:37:25 +0200

    feat: add carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
 const CustomError = require("../extensions/custom-error");
-
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!arguments.length || !sampleActivity || !+sampleActivity ||
+    typeof sampleActivity !== 'string' || sampleActivity < 0 ||
+    sampleActivity > MODERN_ACTIVITY || +sampleActivity > 15) {
+    return false;
+  }
+  if ( typeof sampleActivity == 'string' && sampleActivity.trim().length === 0) {
+    return false;
+  }
+
+  let k = Math.LN2 / HALF_LIFE_PERIOD;
+  let ACTIVITY = Math.log(MODERN_ACTIVITY / sampleActivity);
+  let year = Math.ceil(ACTIVITY / k);
+
+  return year;
 };
