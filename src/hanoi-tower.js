fcccb36fa4fe35824c49c5ec78b9557d68283d41
Irina Giruta <irina.giruta@gmail.com>
2021-03-23 23:22:49 +0200

    feat: add hanoi-tower functionality

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  //const turns = Hanoi(disksNumber, '1','2','3');
+  const turns = (2 ** disksNumber) - 1;
+  const seconds = Math.floor(turns * 3600 / turnsSpeed);
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return { turns, seconds }
 };
+
+// let count=0;
+// function Hanoi(n,a,b,c){
+//   if (n > 0) {
+//     Hanoi(n-1, a, c, b);
+//     count++;
+//     Hanoi(n-1, c, b, a);
+//   }
+//   return count;
+// }
